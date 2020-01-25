53e3eee3c106d3d8b73864db2d56d77101ad9f2d
GarretKing <garret.king95@gmail.com>
2020-01-24 20:33:12 -0500

    updated final calculator

---
+++ Garret new calculator/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public string Operation { get; private set; }
+        public double FirstNumber { get; private set; }
+
+        public Form1()
+        {
+            double FirstNumber;
+            string Operation;
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button14_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "3";
+            }
+        }
+
+        private void Seven_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "7";
+            }
+        }
+
+        private void One_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "1";
+            }
+        }
+
+        private void Eight_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "8";
+            }
+        }
+
+        private void Nine_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "9";
+            }
+        }
+
+        private void Four_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "4";
+            }
+        }
+        private void Five_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "5";
+            }
+
+        }
+
+        private void Six_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "6";
+            }
+        }
+
+        private void Two_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "2";
+            }
+        }
+
+        private void Zero_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "0";
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+             
+            FirstNumber = Convert.ToDouble(textBox1.Text);         
+            textBox1.Text = "0";
+            Operation = "+";
+        }
+
+        private void Substract_Click(object sender, EventArgs e)
+        {
+            FirstNumber = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+            Operation = "-";
+
+        }
+
+        private void Multiply_Click(object sender, EventArgs e)
+        {
+            {
+                FirstNumber = Convert.ToDouble(textBox1.Text);
+                textBox1.Text = "0";
+                Operation = "*";
+            }
+        }
+
+        private void Divide_Click(object sender, EventArgs e)
+        {
+            {
+                FirstNumber = Convert.ToDouble(textBox1.Text);
+                textBox1.Text = "0";
+                Operation = "/";
+            }
+        }
+
+        private void Equals_Click(object sender, EventArgs e)
+        {
+            {
+                double SecondNumber;
+                double Result;
+
+                SecondNumber = Convert.ToDouble(textBox1.Text);
+
+                if (Operation == "+")
+                {
+                    Result = (FirstNumber + SecondNumber);
+                    textBox1.Text = Convert.ToString(Result);
+                    FirstNumber = Result;
+                }
+                if (Operation == "-")
+                {
+                    Result = (FirstNumber - SecondNumber);
+                    textBox1.Text = Convert.ToString(Result);
+                    FirstNumber = Result;
+                }
+                if (Operation == "*")
+                {
+                    Result = (FirstNumber * SecondNumber);
+                    textBox1.Text = Convert.ToString(Result);
+                    FirstNumber = Result;
+                }
+                if (Operation == "/")
+                {
+                    if (SecondNumber == 0)
+                    {
+                        textBox1.Text = "Cannot divide by zero";
+
+                    }
+                    else
+                    {
+                        Result = (FirstNumber / SecondNumber);
+                        textBox1.Text = Convert.ToString(Result);
+                        FirstNumber = Result;
+                    }
+                }
+            }
+
+
+        }
+
+        private void Button12_Click(object sender, EventArgs e)
+        {
+            {
+                textBox1.Text = "0";
+            }
+        }
+    }
+}
+
+
---
+++ Garret new calculator/Form3.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            {
+
+                if (textBox1.Text == "0" && textBox1.Text != null)
+                {
+                    textBox1.Text = "1";
+                }
+                else
+                {
+                    textBox1.Text = textBox1.Text + "1";
+                }
+            }
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            {
+
+                if (textBox1.Text == "0" && textBox1.Text != null)
+                {
+                    textBox1.Text = "4";
+                }
+                else
+                {
+                    textBox1.Text = textBox1.Text + "4";
+                }
+            }
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            {
+
+                if (textBox1.Text == "0" && textBox1.Text != null)
+                {
+                    textBox1.Text = "5";
+                }
+                else
+                {
+                    textBox1.Text = textBox1.Text + "5";
+                }
+            }
+        }
+
+        private void Button11_Click(object sender, EventArgs e)
+        {
+            {
+
+                if (textBox1.Text == "0" && textBox1.Text != null)
+                {
+                    textBox1.Text = "7";
+                }
+                else
+                {
+                    textBox1.Text = textBox1.Text + "7";
+                }
+            }
+        }
+
+        private void Button12_Click(object sender, EventArgs e)
+        {
+            {
+
+                if (textBox1.Text == "0" && textBox1.Text != null)
+                {
+                    textBox1.Text = "8";
+                }
+                else
+                {
+                    textBox1.Text = textBox1.Text + "8";
+                }
+            }
+        }
+
+        private void Button13_Click(object sender, EventArgs e)
+        {
+            {
+
+                if (textBox1.Text == "0" && textBox1.Text != null)
+                {
+                    textBox1.Text = "9";
+                }
+                else
+                {
+                    textBox1.Text = textBox1.Text + "9";
+                }
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double Firstnumber;
+            double.TryParse(textBox1.Text, out Firstnumber);
+            double kilo = 1.60934;
+            double answer = Firstnumber * kilo;
+
+            textBox1.Text = answer.ToString();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            {
+                double Firstnumber;
+                double.TryParse(textBox1.Text, out Firstnumber);
+                double mile = 0.60934;
+                double answer = Firstnumber * mile;
+
+                textBox1.Text = answer.ToString();
+            }
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            {
+
+                if (textBox1.Text == "0" && textBox1.Text != null)
+                {
+                    textBox1.Text = "2";
+                }
+                else
+                {
+                    textBox1.Text = textBox1.Text + "2";
+                }
+            }
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            {
+
+                if (textBox1.Text == "0" && textBox1.Text != null)
+                {
+                    textBox1.Text = "3";
+                }
+                else
+                {
+                    textBox1.Text = textBox1.Text + "3";
+                }
+            }
+        }
+
+        private void Button10_Click(object sender, EventArgs e)
+        {
+            {
+
+                if (textBox1.Text == "0" && textBox1.Text != null)
+                {
+                    textBox1.Text = "6";
+                }
+                else
+                {
+                    textBox1.Text = textBox1.Text + "6";
+                }
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            {
+                double Firstnumber;
+                double.TryParse(textBox1.Text, out Firstnumber);
+                double kilo = 0.453592;
+                double answer = Firstnumber * kilo;
+
+                textBox1.Text = answer.ToString();
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            {
+                double Firstnumber;
+                double.TryParse(textBox1.Text, out Firstnumber);
+                double pound = 2.20462;
+                double answer = Firstnumber * pound;
+
+                textBox1.Text = answer.ToString();
+            }
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button14_Click(object sender, EventArgs e)
+        {
+            {
+                {
+                    textBox1.Text = "0";
+                }
+            }
+        }
+    }
+}
+
